3692026302ccf7e35574c919f309959567134948
Ahmet Özkan <ahmedozkan_1@hotmail.com>
2019-05-15 11:28:53 +0200

    made the game better but still not done

--- mastermind.py
+++ mastermind.py
@@ -1,73 +1,51 @@
-from random import randint
+import random
 
+print(" --- MASTERMIND --- \n")
+print("Guess the secret color code in as few tries as possible.\n")
+print("Please, enter your color code.\nYou can use numbers")
 
-print('Welcome to my Mastermind game, I will generate 4 numbers. Can you guess the order.')
-print('This is how the game works, if you get an C it means you are correct')
-print('If you get an I it means you are correct but in the wrong place')
-print('If you get an N it means you are incorrect')
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+attempts = 0
+game = True
 
+number_code = random.sample(numbers, 4)
+print(number_code)
 
-Number1 = randint(1, 9)
-Number2 = randint(1, 9)
-Number3 = randint(1, 9)
-Number4 = randint(1, 9)
+while game:
+    correct_number = ""
+    guessed_number = ""
+    player_guess = input().upper()
+    attempts += 1
 
-c = 1
+    if len(player_guess) != len(number_code):
+        print("\nThe secret code has exactly four numbers. I know, you can count to four. Try again!")
+        continue
 
-while True:
+    if correct_number != "IIII":
+        for i in range(4):
+            if player_guess[i] == number_code[i]:
+                correct_number += "C"
+            if player_guess[i] != number_code[i] and player_guess[i] in number_code:
+                guessed_number += "N"
+        print(correct_number + guessed_number + "\n")
 
-    print(Number1, Number2, Number3, Number4)
+    if correct_number == "CCCC":
+        if attempts == 1:
+            print("Wow! You guessed at the first attempt!")
+        else:
+            print("Well done... You needed " + str(attempts) + " attempts to guess.")
+        game = False
 
-    guessA = input("Guess the first number: ")
-    guessB = input("Guess the second number: ")
-    guessC = input("Guess the third number: ")
-    guessD = input("Guess the fourth number: ")
+    if attempts >= 1 and attempts < 6 and correct_number != "NNNN":
+        print("Next attempt: ")
+    elif attempts >= 6:
+        print("You didn't guess! The secret code was: " + str(number_code))
 
-    guessA = int(guessA)
-    guessB = int(guessB)
-    guessC = int(guessC)
-    guessD = int(guessD)
-
-    NumbersWrong = 0
-
-    if guessA == Number1:
-        print("C")
-    elif guessA == Number2 or guessA == Number3 or guessA == Number4:
-        print("I")
-        NumbersWrong += 1
-    else:
-        print("N")
-        NumbersWrong += 1
-
-    if guessB == Number2:
-        print("C")
-    elif guessB == Number1 or guessB == Number3 or guessB == Number4:
-        print("I")
-        NumbersWrong += 1
-    else:
-        print("N")
-        NumbersWrong += 1
-
-    if guessC == Number3:
-        print("C")
-    elif guessC == Number1 or guessC == Number4 or guessC == Number2:
-        print("I")
-        NumbersWrong += 1
-    else:
-        print("N")
-        NumbersWrong += 1
-
-    if guessD == Number4:
-        print("C")
-    elif guessD == Number1 or guessD == Number2 or guessD == Number3:
-        print("I")
-        NumbersWrong += 1
-    else:
-        print("N")
-        NumbersWrong += 1
-
-    if NumbersWrong == 0:
-        print("Well done! you had 0 numbers wrong")
-        break
-    else:
-        print("You had " + str(NumbersWrong) + " wrong, try again")
+    while game == False:
+        finish_game = input("\nDo you want to play again (Y/N)?").upper()
+        attempts = 0
+        if finish_game == "N":
+            print("Thanks for the game! Bye, bye!")
+        elif finish_game == "Y":
+            game = True
+            print("So, let's play again... Guess the secret code: ")
